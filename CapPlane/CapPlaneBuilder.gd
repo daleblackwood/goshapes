aa4b5bfee42439bb1eaef75c9e2d4e04812f6254
Dale Williams <dale@dalejwilliams.com>
2022-02-07 21:20:35 +1000

    plane cap style

--- CapPlane/CapPlaneBuilder.gd
+++ CapPlane/CapPlaneBuilder.gd
@@ -2,7 +2,7 @@ tool
 extends CapBuilder
 
 func build(style, path: PathData):
-	var gs = 1.0
+	var gs := 1.0
 	if style != null and style.grid_size > 0.0:
 		gs = style.grid_size
 		
@@ -19,37 +19,29 @@ func build(style, path: PathData):
 		pmin.z = min(p.z, pmin.z)
 		pmax.z = max(p.z, pmax.z)
 
-	var cols = floor((pmax.x - pmin.x) / gs) + 1
-	var rows = floor((pmax.z - pmin.z) / gs) + 1
+	var coln = floor((pmax.x - pmin.x) / gs) + 2
+	var rown = floor((pmax.z - pmin.z) / gs) + 2
 		
 	var meshes = []
-		
-	for cy in range(rows):
-		for cx in range(cols):
-			var a = Vector2(cx * gs + pmin.x, cy * gs + pmin.z)
-			var b = a + Vector2(gs, 0)
-			var c = a + Vector2(gs, gs)
-			var d = a + Vector2(0, gs)
-			var plane = Plane(
-				Vector3(a.x, 100, a.y),
-				Vector3(b.x, 100, b.y),
-				Vector3(c.x, 100, c.y)
-			)
-			var clipped = Geometry.clip_polygon(path.points, plane)
-			if clipped.size() > 0:
-				meshes.append(MeshUtils.make_cap(clipped))
-			#var d = a + Vector2(0, gs)
-			#var corners = PoolVector2Array([a, b, c, d])
-			#var icount = inside_count(points, corners)
-			#if icount == 4:
-			#	meshes.append(MeshUtils.make_cap(to_vec3s(corners)))
-			#	continue
-			#var corners3 = to_vec3s(corners)
-			#meshes.append(MeshUtils.make_cap(corners3))
-			#var intersections = Geometry.intersect_polygons_2d(points, corners)
-			#for intersection in intersections:
-			#	var cap_points = to_vec3s(intersection)
-			#	meshes.append(MeshUtils.make_cap(cap_points))
+	
+	var cols = []
+	for gx in range(coln):
+		for gz in range(rown):
+			var clipx = gx * gs + pmin.x
+			var clipped = Geometry.clip_polygon(path.points, Plane(Vector3.LEFT, -clipx))
+			if clipped.size() < 1:
+				continue
+			clipped = Geometry.clip_polygon(clipped, Plane(Vector3.RIGHT, clipx + gs))
+			if clipped.size() < 1:
+				continue
+			var clipz = gz * gs + pmin.z
+			clipped = Geometry.clip_polygon(clipped, Plane(Vector3.FORWARD, -clipz))
+			if clipped.size() < 1:
+				continue
+			clipped = Geometry.clip_polygon(clipped, Plane(Vector3.BACK, clipz + gs))
+			if clipped.size() < 1:
+				continue
+			meshes.append(MeshUtils.make_cap(clipped))
 			
 	var set = MeshUtils.combine_sets(meshes)
 	set.material = style.material
